f57e0fb0d4ec228e804a1694d8341c80b9524ea4
ChiShiang <hyaline0317@gmail.com>
2016-01-20 15:31:56 +0800

    add new function svm_trainModel_test

--- bin/classifier.py
+++ bin/classifier.py
@@ -11,15 +11,24 @@ class Classifier(object):
 	def __init__(self):
 		super(Classifier, self).__init__()
 		
-	def svm_process_train(self, training_data):
+	def svm_process_train(self, training_data, word_vector_base):
+		# training_data 為tweet元件，因此可以從Feature 與 SA取得你的特徵與正確答案
+
+	def svm_trainModel_test(self, model, testing_data):
+		if type(model) is str:
+			# 讀取外部以訓練好的分類氣
+			svm_clf = joblib.load(model_pkl)
+		else:
+			# 讀取已經訓練好的分類器模型
+			svm_clf = model
+
+		# 載入testing data進行預測
+		testing_result = svm_clf.predict(testing_data)
+		
+		return testing_result
+
+
+	def nbc_process_train(self, training_data, word_vector_base):
 		# training_data 為tweet元件，因此可以從Feature 與 SA取得你的特徵與正確答案
 		
-
-	def nbc_process_train(self, training_data):
-		# training_data 為tweet元件，因此可以從Feature 與 SA取得你的特徵與正確答案
-		
-
-	def svm_trainModel_test(self, model_file, testing_data):
-
-
 	def nbc_trainModel_test(self, model_file, testing_data):
