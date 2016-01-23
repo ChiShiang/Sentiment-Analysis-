ebf87dbf1198fbfac3b9eb98d6d81788e33092e0
ChiShiangWang <hyaline0317@gmail.com>
2016-01-23 15:06:31 +0800

    fix the bug, and relpace the nltk.NavieBayesClassification to scikit.NavieBayes

--- bin/classifier.py
+++ bin/classifier.py
@@ -2,6 +2,7 @@ import pickle
 
 # scikit learn
 from sklearn import svm
+from sklearn.naive_bayes import GaussianNB as gnbc
 from sklearn.externals import joblib
 
 # nltk package
@@ -12,33 +13,32 @@ class Classifier(object):
 	"""docstring for Classifier"""
 	def __init__(self):
 		super(Classifier, self).__init__()
-		
+		SVM = svm.SVC(kernel = 'rbf')
+		NBC = gnbc()
+		setattr(svm, 'svm_clf', SVM)
+		setattr(gnbc, 'nbc_clf', NBC)
+	# ===========================SVM Classification===========================
+
 	def svm_process_train(self, training_data, s_gamma = 0.10000000001, s_C = 1.0):
 		# training_data 為tweet元件，因此可以從Feature 與 SA取得你的特徵與正確答案
-		svm_clf = svm.SVC(kernel = 'rbf', gamma = s_gamma, C = s_C).fit(training_data.X, training_data.Y)
-		
+		self.svm_clf = svm.SVC(kernel = 'rbf', gamma = s_gamma, C = s_C).fit(training_data['data'],
+		                                                                training_data['standard'])	
 		# 將訓練好的classifier model輸出 
-		joblib.dump(svm_clf, './model/svm_clf.pkl')	
-		setattr("svm_clf", svm_clf)
+		joblib.dump(self.svm_clf, './model/svm_clf.pkl')
 
 	def load_svm_clf(self, model):
 		# 讀取外部以訓練好的分類器
-		svm_clf = joblib.load(model_pkl)
-		setattr('svm_clf', svm_clf)
+		self.svm_clf = joblib.load(model)
 
+	# ===========================Naive Bayes Classification===================
 	def nbc_process_train(self, training_data):
 		# training_data 為tweet元件，因此可以從Feature 與 SA取得你的特徵與正確答案
-		nbc_clf = nbc.train(training_data)
-		
-		# 將訓練好的classifier model輸出 
-		with open("./model/nbc_clf", 'wb') as nbc_clf_file:
-			pickle.dump(nbc_clf, nbc_clf_file)
+		self.nbc_clf = gnbc().fit(training_data['data'], training_data['standard'])
+		# 將訓練好的classifier model輸出
+		joblib.dump(self.nbc_clf, './model/nbc_clf.pkl')
 
-		setattr("nbc_clf", nbc_clf)
+	def load_nbc_clf(self, model):
+		# 讀取外部以訓練好的分類器
+		self.nbc_clf = joblib.load(model)
+
 		
-	def load_nbc_clf(self, model, data):
-		# 讀取外部以訓練好的分類氣	
-		with open(model, 'rb') as clf_file:
-			nbc_clf = pickle.load(clf_file)
-		
-		setattr('nbc_clf', nbc_clf)
